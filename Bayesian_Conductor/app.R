8d70ff08d70652b49feaf511e528d2b5021464b2
mxl <mx.meidinger@gmail.com>
2019-03-25 01:20:17 +0100

    First working data save/load version.

--- Bayesian_Conductor/app.R
+++ Bayesian_Conductor/app.R
@@ -1,11 +1,6 @@
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(ggplot2)
@@ -14,8 +9,9 @@ library(hashids)
 
 # for online deployment, one has to use remote storing, an article about which
 # can be found here: https://shiny.rstudio.com/articles/persistent-data-storage.html#basic
-
-saveData <- function(in_data, fileName, dat_ID) {
+#####
+#Functions
+saveData <- function(in_data, current_data, fileName, dat_ID) {
   out_data <- data.frame("ID" = dat_ID,
                          "date_start" = in_data$date_start, 
                          "date_stop" = in_data$date_stop, 
@@ -24,35 +20,38 @@ saveData <- function(in_data, fileName, dat_ID) {
                          "total_rides" = in_data$total_rides,
                          "faction" = in_data$number_inspections /in_data$total_rides,
                          "empirical_b" = 0)
-  print("Current ID: ", out_data$ID)
-  # Write the file to the local system
-  write.table(out_data, file=fileName, append = TRUE, sep =",", col.names = FALSE)
+  #combine the data frames
+  if (!is.null(current_data)){
+    latest_data <- rbind(current_data, out_data)
+    write.csv(latest_data, fileName, row.names = FALSE)
+  } else {
+    print("First save")
+    # Write the file to the local system
+    write.csv(out_data, fileName, row.names = FALSE)
+  }
 }
 
 loadData <- function(fileName) {
-    # Read all the files into a list
-    ldata <- read.csv(fileName, stringsAsFactors = FALSE, header = TRUE)
-    return(ldata)
+    if (file.exists(fileName)){
+      ldata <- as.data.frame(read.csv(fileName, stringsAsFactors = FALSE, header = TRUE))
+      #recover correct date time
+      ldata$date_start <- as.Date(ldata$date_start, origin = "1970-01-01")
+      ldata$date_stop <- as.Date(ldata$date_stop, origin = "1970-01-01")
+      return(ldata)
+    } else {
+      cat("No file: ", fileName)
+    }
 }
 
+#####
 
-#Upfront data fetching from working directory
+#Upfront working directory setup
 data_filename <- "data/conductor.csv"
-
-
-if (dir.exists("data")) {
-  if (!file.exists("data/conductor.rds")){
-    cdata <- data.frame("ID", "date_start", "date_stop", "tr_type", "number_inspections", "total_rides", "fraction", "empirical_b")
-    write.csv(cdata, file=data_filename)
-  }
-} else {
+if (!dir.exists("data")) {
   dir.create("data")
-  cdata <- data.frame("ID", "date_start", "date_stop", "tr_type", "number_inspections", "total_rides", "fraction", "empirical_b")
-  write.csv(cdata, file=data_filename)
 }
 
-# Define the fields we want to save from the form - deprecated
-#fields <- c("date_start","date_stop", "tr_type", "total_rides", "number_inspections")
+#####
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -62,7 +61,7 @@ ui <- fluidPage(
    sidebarLayout(
       sidebarPanel(
         h4("Insert your data"),
-        p("Create an individual data record where you enter your experienced ticket inspections over
+        p("Create an individual data record where you enter your experienced ticket inspections during
           your total number of train rides."),
         selectInput("tr_type", 
                     label="Select the train type you used: ", 
@@ -88,8 +87,12 @@ ui <- fluidPage(
    )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
+  
+  # set up reactive values, i.e. dynamic values
+  values <- reactiveValues(counter = 0, current_dat = NULL)
+  
   # Whenever a field is filled, aggregate all form data
   formData <- reactive({
     newdata <- list(date_start= as.Date(input$date_range[1], origin = "1970-01-01"), 
@@ -97,26 +100,33 @@ server <- function(input, output) {
                     tr_type= as.integer(input$tr_type), 
                     number_inspections= input$number_inspections, 
                     total_rides= input$total_rides)
-    newdata
+    newdata #return
   })
 
-  values <- reactiveValues(counter = 0, current_dat = NULL)
-  
   # When the Submit button is clicked, save the form data
   observeEvent(input$Submit, {
     values$counter <- values$counter + 1
-    saveData(formData(), fileName = data_filename, dat_ID= values$counter)
-    current_dat <- as.data.frame(loadData(fileName = data_filename))
-    output$hello_world <- renderText("Thank you for your data!, blabla")
+    saveData(formData(), current_data =  values$current_dat, fileName = data_filename, dat_ID = values$counter)
+    #get latest data after saving
+    values$current_dat <- loadData(fileName = data_filename)
+    output$hello_world <- renderText("Thank you for your data!")
   })
   
   output$type_selected <- renderText({paste("You have selected type: ", input$tr_type)})
-  #output$date_range_sel <- renderText({paste(as.character(input$date_range), collapse=" to: ")})
   output$date_range_sel <- renderText({paste("dates from: ", as.Date(input$date_range[1], origin = "1970-01-01"), " until: ", as.Date(input$date_range[2], origin = "1970-01-01"))})
   output$total_rides <- renderText({paste("total number of train rides:", input$total_rides)})
   output$total_inspections <- renderText({paste("total number of ticket inspections: ", input$number_inspections)})
+  
   #render Table
   output$responses <- DT::renderDataTable({
+    print("Render load of data")
+    #dynamically input this new data to the reactive environment
+    values$current_dat <- loadData(data_filename)
+    #load latest ID from data 
+    if (values$current_dat$ID[nrow(values$current_dat)]>1){
+      values$counter <- values$current_dat$ID[nrow(values$current_dat)]
+    }
+    #render current data
     input$Submit
     loadData(fileName = data_filename)
     })     
